9aa6bcfee429b94cdc2627b223da8e18b9ab7123
ElProfetaMohammed <ElProfetaMohammed@gmail.com>
2025-09-06 11:44:22 -0700

    Inicializacion de proyecto con NEXTJS/Typescript/SHADCN

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import pluginNext from "@next/eslint-plugin-next";
+import parser from "@typescript-eslint/parser"; // optional
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+    },
   },
 ];
-
-export default eslintConfig;
